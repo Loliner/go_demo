61c06dcc8dc1cd61f042c347801a097c51fd11ac
zhengjiu <345394613@qq.com>
2026-04-12 22:10:25 +1000

    feat: http and json

--- 13_http/exercise.go
+++ 13_http/exercise.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
 
 // ============================================================
 // 练习：实现一个简单的 TODO REST API
@@ -25,7 +30,72 @@ import "fmt"
 //    curl http://localhost:8080/todos
 
 // TODO: 在这里写你的代码
+type Todo struct {
+	ID    int    `json:"id"`
+	Title string `json:"title"`
+	Done  bool   `json:"done"`
+}
+
+var database = map[int]Todo{}
+
+type Request struct {
+	Title string `json:"title"`
+}
+
+var nextID int
+
+func newID() int {
+	nextID++
+	return nextID
+}
+
+func getTodos() (todos []Todo) {
+	for _, value := range database {
+		todos = append(todos, value)
+	}
+	if todos == nil {
+		todos = []Todo{}
+	}
+	return todos
+}
+
+func setTodo(title string) (todos []Todo) {
+	todo := Todo{
+		ID:    newID(),
+		Title: title,
+		Done:  false,
+	}
+
+	database[todo.ID] = todo
+	fmt.Println(database)
+
+	todos = append(todos, todo)
+	return todos
+}
+
+func todoHandler(response http.ResponseWriter, request *http.Request) {
+	todos := []Todo{}
+	switch request.Method {
+	case http.MethodGet:
+		todos = getTodos()
+	case http.MethodPost:
+		var req Request
+		if err := json.NewDecoder(request.Body).Decode(&req); err != nil {
+			http.Error(response, "invalid json", http.StatusBadRequest)
+			return
+		}
+		todos = setTodo(req.Title)
+	}
+
+	response.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(response).Encode(todos)
+}
 
 func exercise() {
 	fmt.Println("=== Exercise: TODO REST API ===")
+
+	http.HandleFunc("/todos", todoHandler)
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
 }
